92d88b31ffc9ed677ca6c71df0ebb0b1501b6f62
mikefab <mikefabrikant@gmail.com>
2017-10-26 16:56:20 -0400

    config sample

---
+++ config-sample.js
@@ -0,0 +1,16 @@
+module.exports = {
+  db_schools: {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'project-connect_development',
+    password: '',
+    port: 5432,
+  },
+  db_countries: {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'all_countries_one_table',
+    password: '',
+    port: 5432,
+  }
+}
